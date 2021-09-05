eae6e3191b209cf234b2b209bc3e5ab55ce53a16
Yanto Lee <yanto3528@gmail.com>
2021-09-05 19:57:30 +0700

    setup linter to ignore dist and node_modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'rollup';
-import { babel } from '@rollup/plugin-babel';
-import { visualizer } from 'rollup-plugin-visualizer';
-import size from 'rollup-plugin-sizes';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import peerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
+import { defineConfig } from 'rollup'
+import { babel } from '@rollup/plugin-babel'
+import { visualizer } from 'rollup-plugin-visualizer'
+import size from 'rollup-plugin-sizes'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import peerDepsExternalPlugin from 'rollup-plugin-peer-deps-external'
 
 // Will use this later on
 // import { terser } from 'rollup-plugin-terser';
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
 const config = defineConfig({
   input: 'src/index.ts',
@@ -44,6 +44,6 @@ const config = defineConfig({
       brotliSize: true,
     }),
   ],
-});
+})
 
-export default config;
+export default config
